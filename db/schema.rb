5462a83d22f954f94ff2e94e5be8713c9a2537c1
letizia obiglio <letizia.obiglio@gmail.com>
2019-08-19 18:28:24 +0200

    add avatar default to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_19_135355) do
+ActiveRecord::Schema.define(version: 2019_08_19_162448) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,7 +75,7 @@ ActiveRecord::Schema.define(version: 2019_08_19_135355) do
     t.datetime "updated_at", null: false
     t.string "name"
     t.string "address"
-    t.string "avatar"
+    t.string "avatar", default: "https://res.cloudinary.com/dcezznavh/image/upload/v1566231827/sa4zukx9nflvkeg5510x.jpg"
     t.integer "role", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
