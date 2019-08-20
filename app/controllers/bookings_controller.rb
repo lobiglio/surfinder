761df1d0740522437340f8377b91c88bf0f68dd1
letizia obiglio <letizia.obiglio@gmail.com>
2019-08-20 12:12:02 +0200

    pack créé pour l'école test afin de les add au formulaire new booing

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,10 +1,24 @@
 class BookingsController < ApplicationController
   def index
+    @booking = Booking.all
   end
 
   def new
+    @school = School.find(params[:school_id])
+    @booking = Booking.new
   end
 
   def create
+    @booking = Booking.new(booking_params)
+    @school = School.find(params[:school_id])
+    @booking.school = @school
+    @booking.save
+    redirect_to bookings_path
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:begin_at, :end_at)
   end
 end
