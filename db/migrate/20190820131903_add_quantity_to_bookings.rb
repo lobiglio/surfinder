117394adae787dd33e263ef7ef1ccab4ea03d544
letizia obiglio <letizia.obiglio@gmail.com>
2019-08-20 15:32:09 +0200

    formulaire done

--- db/migrate/20190820131903_add_quantity_to_bookings.rb
+++ db/migrate/20190820131903_add_quantity_to_bookings.rb
@@ -1,5 +1,5 @@
 class AddQuantityToBookings < ActiveRecord::Migration[5.2]
   def change
-    add_column :bookings, :quantity, :integer
+    add_column :bookings, :quantity, :integer, default: 0
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_20_132332) do
+ActiveRecord::Schema.define(version: 2019_08_20_132953) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,7 +30,7 @@ ActiveRecord::Schema.define(version: 2019_08_20_132332) do
     t.bigint "pack_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "quantity", default: 0
+    t.integer "quantity"
   end
 
   create_table "packs", force: :cascade do |t|
