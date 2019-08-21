6dd2ae870c84698924d883bf4efd725eed84cef8
letizia obiglio <letizia.obiglio@gmail.com>
2019-08-21 18:01:26 +0200

    pack/application

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,9 +2,7 @@ import "bootstrap";
 import { initAutocomplete, initAutocomplete2 } from '../plugins/init_autocomplete';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import "../plugins/flatpickr"
-
 import { initMapbox } from '../plugins/init_mapbox';
-import { initAutocomplete } from '../plugins/init_autocomplete';
 import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 
 
