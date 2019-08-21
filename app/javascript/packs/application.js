45ebfc47e2b82e1059ed6050971b2615a9cbc5ed
Mickael Delpech <mickael.delpech@gmail.com>
2019-08-21 15:03:06 +0200

    transfert address

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,3 +10,20 @@ import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 initMapbox();
 initAutocomplete();
 
+
+// récupérer ton input et surtout le html qu'est dedans sur home
+
+// addEventListener sur le bouton rechercher
+
+// enregistrer cet HTML dans document.cookie - console.log cookie
+
+// récupèrer nouvel input
+
+// innerHTML = et lire le cookie pour en récupérer juste la valeur que tu veux
+
+const address = document.getElementById("valid-address-btn");
+address.addEventListener("click", (event) => {
+  input = document.querySelectorAll("#form-search-school")
+  alert('hi');
+  console.log(input);
+});
