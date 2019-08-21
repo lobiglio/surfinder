805b96c24325a11bb2db53df12a8bc2134a523ae
Mickael Delpech <mickael.delpech@gmail.com>
2019-08-21 08:29:58 +0200

    index V1

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -1,7 +1,17 @@
 class SchoolsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
   def index
-    @schools = School.all
+    @schools = School.geocoded
+
+    @markers = @schools.map do |school|
+      {
+        lat: school.latitude,
+        lng: school.longitude,
+        infoWindow: render_to_string(partial: "info_window", locals: { school: school }),
+        image_url: helpers.asset_url('logo.png')
+      }
+
+    end
   end
 
   def show
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,4 +1,11 @@
 import "bootstrap";
-import { initAutocomplete } from '../plugins/init_autocomplete';
+import 'mapbox-gl/dist/mapbox-gl.css';
 
+import { initMapbox } from '../plugins/init_mapbox';
+import { initAutocomplete } from '../plugins/init_autocomplete';
+import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
+
+
+initMapbox();
 initAutocomplete();
+
