dde6171080dd9b14770ac20b56597f117da8d79a
James Arbacette <jamesarbacette@club-internet.fr>
2019-08-22 10:45:14 +0200

    servie file added

--- app/Services/surf_forecast_service.rb
+++ app/Services/surf_forecast_service.rb
@@ -1,18 +1,21 @@
 class SurfForecastService
   require "http"
   def initialize(spot_id)
-    @url = "http://magicseaweed.com/api/ENV[API KEY]/forecast/?spot_id=#{spot_id}&fields=timestamp,swell.primary,wind.*,condition.temperature,charts"
+    #@url = "http://magicseaweed.com/api/ENV[API KEY]/forecast/?spot_id=#{spot_id}&fields=timestamp,swell.primary,wind.*,condition.temperature,charts"
+    @url = "http://magicseaweed.com/api/ENV[STORMGLASS_API_KEY]/forecast/?spot_id=#{spot_id}&fields=timestamp,swell.primary,wind.*,condition.temperature,charts"
   end
 
   def call
-    @forecasts = HTTP.get(@url).to_s
+    lat = 43.6649;
+    lng = 17.8081;
+    @weather = HTTP.get("https://api.stormglass.io/v1/weather/point?lat=${43.6649}&lng=${-1.3971}").to_s
 
-    {
-      timestamp: @forecast[0][:timestamp],
-      swell: @forecast[0][:swell.primary],
-      wind: @forecast[0][:wind],
-      condition: @forecast[0][:condition.temperature],
-      chart: @forecast[0][:charts]
-    }
+    # {
+    #   timestamp: @forecast[0][:timestamp],
+    #   swell: @forecast[0][:swell.primary],
+    #   wind: @forecast[0][:wind],
+    #   condition: @forecast[0][:condition.temperature],
+    #   chart: @forecast[0][:charts]
+    # }
   end
 end
