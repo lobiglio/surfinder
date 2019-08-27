541e83fae2c69dd4fca15130c95c1456d5d51d89
Luix Roussarie <luixitos@hotmail.com>
2019-08-27 11:59:07 +0200

    jstitre

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,8 +4,14 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 import "../plugins/flatpickr"
 import { initMapbox } from '../plugins/init_mapbox';
 import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
+import { loadDynamicBannerText } from '../components/banner';
+
 
 
 initMapbox();
 initAutocomplete();
 initAutocomplete2();
+
+if (window.location.href === 'http://localhost:3000/' || window.location.href === 'https://surfinder-276.herokuapp.com/') {
+  loadDynamicBannerText();
+}
