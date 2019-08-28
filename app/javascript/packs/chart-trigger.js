1d734eff4a8148c677fdbab4b517e50fa377e82a
James Arbacette <jamesarbacette@club-internet.fr>
2019-08-28 10:18:21 +0200

    data charts finished

--- app/javascript/packs/chart-trigger.js
+++ app/javascript/packs/chart-trigger.js
@@ -1,6 +1,5 @@
 const chartTrigger = () => {
 
-  // définir une constant js qui correspond à ta div weather-chart
   const weather_chart = document.querySelector("#weather");
 
   const tabs = document.querySelectorAll(".select-tabs");
@@ -13,19 +12,6 @@ const chartTrigger = () => {
         weather_chart.classList.add('d-none'); }
     })
   })
-
-  // chartShow.addEventListener("click", (event) => {
-  //   // au clic, je veux enlever la class 'd-none' de ma div 'weather-chart'
-  //   // event.currentTarget.toggle("weather")
-  //   weather_chart.hidden = false;
-  // });
-
-  // // ajouter un event listenenr sur les autres onglets
-  // chartHide.addEventListener("click", (event) => {
-  //   // au clic, je veux rajouter la class 'd-none' de ma div 'weather-chart'
-  //   // event.currentTarget.classlist.toggle("weather")
-  //   weather_chart.hidden = true;
-  // });
 };
 
 export { chartTrigger }
